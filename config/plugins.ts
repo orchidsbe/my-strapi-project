ed9e86a22686800d56f56b7798df46d9984ca2c8
Islam <orchidsbe@gmail.com>
2025-11-19 16:39:47 +0300

    бновлен конфиг почты

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: 'smtp',  // <- изменено
+      provider: 'smtp',
       providerOptions: {
-        host: env('SMTP_HOST', 'smtp.interimjob.ru'),
+        host: env('SMTP_HOST', 'mail.interimjob.ru'), // <-- новый сервер
         port: env('SMTP_PORT', 587),
         auth: {
           user: env('SMTP_USERNAME', 'info@interimjob.ru'),
           pass: env('SMTP_PASSWORD'),
         },
-        // secure: false, // включить, если TLS не нужен
+        // secure: false, // оставляем false для STARTTLS на 587
       },
       settings: {
         defaultFrom: env('SMTP_FROM', 'info@interimjob.ru'),
