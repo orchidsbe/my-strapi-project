2ad196283fadcabaa9da11391e2d3f37dac50d34
Islam <orchidsbe@gmail.com>
2025-11-19 11:45:08 +0300

    Настройка email для Strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.interimjob.ru'), 
+        port: env('SMTP_PORT', 587), 
+        auth: {
+          user: env('SMTP_USERNAME', 'info@interimjob.ru'), 
+          pass: env('SMTP_PASSWORD'), 
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM', 'info@interimjob.ru'),
+        defaultReplyTo: env('SMTP_FROM', 'info@interimjob.ru'),
+      },
+    },
+  },
+});
